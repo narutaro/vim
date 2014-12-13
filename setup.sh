c86d183105e8fe0c373c65c70818e5087fb32eaf
dsap <dsap@debian>
2014-12-12 16:33:32 -0800

    symbolic link

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+rm -fr ~/.vim
+rm ~/.vimrc
 
 # Just in case - debian
 sudo apt-get install vim-gui-common -y
@@ -9,26 +11,31 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 echo '" Pathogen' 
-echo "execute pathogen#infect()" >> ~/.vimrc
-echo "syntax on" >> ~/.vimrc
-echo "filetype plugin indent on" >> ~/.vimrc
+echo 'execute pathogen#infect()' >> ~/.vimrc
+echo 'syntax on' >> ~/.vimrc
+echo 'filetype plugin indent on' >> ~/.vimrc
 
 # Install Plugins
 cd ~/.vim/bundle && \
 
 # Nerdtree
 git clone https://github.com/scrooloose/nerdtree.git
+echo '" NerdTree' >> ~/.vimrc
+echo 'nnoremap <silent><C-n> :NERDTreeToggle<CR>' >> ~/.vimrc
 
 # Lightline
 git clone https://github.com/itchyny/lightline.vim.git
-echo "set laststatus=2" >> ~/.vimrc
+echo '" Lightline' >> ~/.vimrc
+echo 'set laststatus=2' >> ~/.vimrc
 
 # indentLine
 git clone https://github.com/Yggdroot/indentLine.git
+ln -s ~/.vim/bundle/indentLine/after/plugin/indentLine.vim ~/.vim/bundle/indentLine.vim
 
 # molokai
 cd ~/.vim && \
 git clone https://github.com/fatih/molokai.git
-cp -r ~/.vim/molokai/colors .
-echo "colorscheme molokai" >> ~/.vimrc
-
+#cp -r ~/.vim/molokai/colors .
+ln -s ~/.vim/molokai/colors ./colors
+echo '" Molokai' >> ~/.vimrc
+echo 'colorscheme molokai' >> ~/.vimrc
